e828a72ecc87769028ef24f1cda8d8d20ce45c99
Denis <krollik1000000@gmail.com>
2019-04-06 13:17:26 +0300

    'solution'

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -24,7 +24,57 @@
  */
 
 function deepEqual(a, b) {
-  // write code here
+  if (typeof (a) === 'number' && typeof (b) === 'number' && a === b) {
+    return true;
+  }
+  if (a === null && b === null) {
+    return true;
+  }
+  if (typeof (a) === 'object' && typeof (b) === 'object') {
+    let str = '';
+    let strTwo = '';
+    let number = 0;
+    let numberTwo = 0;
+    for (let c in a) {
+      if (a[c] === null) {
+        return false;
+      }
+      if (typeof (a[c]) !== 'object') {
+        str += a[c];
+      } else if (typeof (a[c]) === 'object') {
+        for (let e in a[c]) {
+          if (a[c][e] !== null) {
+            number += a[c][e];
+          } else {
+            return false;
+          }
+        }
+      }
+    }
+    for (let d in b) {
+      if (b[d] === null) {
+        return false;
+      }
+      if (typeof (b[d]) !== 'object') {
+        strTwo += b[d];
+      } else {
+        for (let r in b[d]) {
+          if (b[d][r] !== null) {
+            numberTwo += b[d][r];
+          } else {
+            return false;
+          }
+        }
+      }
+    }
+    if (str === strTwo && number === numberTwo) {
+      return true;
+    }
+  }
+  if ((typeof (a) === 'object' && b === null) || (typeof (b) === 'object' && a === null)) {
+    return false;
+  } else {
+    return false;
+  }
 }
-
 module.exports = deepEqual;
