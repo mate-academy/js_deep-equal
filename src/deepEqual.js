ac8f9a2f6519b4144e08839d23d6d4c1d9e2a910
unknown <dumavb777@gmail.com>
2019-04-06 22:30:32 +0300

    'Solution'

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -23,8 +23,55 @@
  *
  */
 
+function isEquals(value1, value2) {
+  if (typeof value1 === typeof value2) {
+    if (value1 === value2) {
+      return true;
+    }
+    return false;
+  }
+  return false;
+}
+
+function isObjectNotNull(elem, elem2) {
+  let result;
+  if (elem === null || elem2 === null) {
+    result = false;
+  } else if (typeof elem === 'object') {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
+}
+
+const objLength = (obj) => Object.keys(obj).length;
+
 function deepEqual(a, b) {
-  // write code here
+  if (isObjectNotNull(a, b)) {
+    if (objLength(a) === objLength(b)) {
+      for (let key in a) {
+        let keyOfA = a[key];
+        let keyOfB = b[key];
+        if (isObjectNotNull(a[key], b[key])) {
+          if (deepEqual(a[key], b[key])) {
+            continue;
+          }
+          return false;
+        }
+        if (Object.keys(b).includes(key)) {
+          if (keyOfA !== keyOfB) {
+            return false;
+          }
+          continue;
+        }
+        return false;
+      }
+      return true;
+    }
+    return false;
+  }
+  return isEquals(a, b);
 }
 
 module.exports = deepEqual;
