86d2bcf9b732d1d4564d41a1bf8f852a9c41b142
Olegus <gulldan1996@gmail.com>
2019-04-07 20:53:52 +0300

    deepEqual))

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -24,7 +24,25 @@
  */
 
 function deepEqual(a, b) {
-  // write code here
+  if (a === b) {
+    return true;
+  } else if ((typeof a === 'object' && a !== null) && (typeof b === 'object' && b !== null)) {
+    if (Object.keys(a).length !== Object.keys(b).length) {
+      return false;
+    }
+    for (var prop in a) {
+      if (b.hasOwnProperty(prop)) {
+        if (!deepEqual(a[prop], b[prop])) {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = deepEqual;
