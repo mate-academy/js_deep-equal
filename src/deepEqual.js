c1a90a1da3c17b34e33b5fd5067ff329cdacbf19
Vitalii <ilovepinkpony1@gmail.com>
2019-04-07 08:59:24 +0300

    solution

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -20,7 +20,27 @@
  * deepEqual({test: {abc: 5}}, {test: {abc: 5, def: 4}}) === false
  */
 function deepEqual(a, b) {
-  // write code here
-}
-
+  if (
+    typeof a === 'object' &&
+    typeof b === 'object' &&
+    a !== null &&
+    b !== null
+  ) {
+    if (Object.keys(a).length !== Object.keys(b).length) {
+      return false;
+    }
+    for (let key in a) {
+      if (!b.hasOwnProperty(key)) {
+        return false;
+      } else if (b.hasOwnProperty(key) && b[key] === a[key]) {
+        continue;
+      } else if (b.hasOwnProperty(key) && b[key] !== a[key]) {
+        return deepEqual(b[key], a[key]);
+      } else return false;
+    }
+    return true;
+  } else {
+    return a === b;
+  }
+};
 module.exports = deepEqual;
