5a846c482d79be084bd4af31498031c52c5e954d
Onisko <Onisko@MacBook.local>
2019-04-07 16:03:25 +0300

    Solution

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -19,8 +19,28 @@
  * deepEqual({test: {abc: 5}}, {test: {abc: 5}}) === true
  * deepEqual({test: {abc: 5}}, {test: {abc: 5, def: 4}}) === false
  */
-function deepEqual(a, b) {
-  // write code here
+function deepEqual(obj1, obj2) {
+  if (obj1 === obj2) {
+    return true;
+  }
+
+  if (obj1 === null || typeof (obj1) !== 'object' ||
+      obj2 === null || typeof (obj2) !== 'object') {
+    return false;
+  }
+
+  var propertiesInObj1 = 0;
+  var propertiesInObj2 = 0;
+  for (var property in obj1) {
+    propertiesInObj1 += 1;
+  }
+  for (property in obj2) {
+    propertiesInObj2 += 1;
+    if (!(property in obj1) || !deepEqual(obj1[property], obj2[property])) {
+      return false;
+    }
+  }
+  return propertiesInObj1 === propertiesInObj2;
 }
 
 module.exports = deepEqual;
