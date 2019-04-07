625a109d49c65d40136bf830ea4574c6ff65373d
valeriitovstyk <valeriitovstyk@gmail.com>
2019-04-07 10:15:07 +0300

    not ready, 10 from 12 tests passed

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -24,7 +24,52 @@
  */
 
 function deepEqual(a, b) {
-  // write code here
+  let typeOfA = typeof (a);
+  let typeOfB = typeof (b);
+
+  if ((typeOfA === typeOfB)) {
+    if ((typeOfA === 'object' && a === null) || typeOfA === undefined || typeOfA !== 'object') {
+      return comparePrimitives(a, b);
+    }
+    return compareObjects(a, b);
+  }
+  console.log('false \t \t they are different types');
+  return false;
+}
+
+function comparePrimitives(a, b) {
+  console.log(a === b);
+  return a === b;
+}
+
+function compareObjects(objA, objB) {
+  if (objA === null || objB === null) {
+    return false;
+  }
+  let propertiesA = Object.keys(objA);
+  let propertiesB = Object.keys(objB);
+
+  if (typeof (objA) === 'object') {
+    if (propertiesA.length === propertiesB.length) {
+      for (var eachProperty of propertiesA) {
+        if (objB.hasOwnProperty(eachProperty) && objB[eachProperty]) {
+          // if (propertiesA[eachProperty] === null || propertiesA[eachProperty] === undefined) {return false;}
+          return compareObjects(objA[eachProperty], objB[eachProperty]);
+        } else {
+          console.log('false \t\t b has no same property as object a');
+          return false;
+        }
+      }
+    } else {
+      console.log('fasle \t \t length of object is not the same therefore they are not equal');
+      return false;
+    }
+  } else {
+    return comparePrimitives(objA, objB);
+  }
 }
 
 module.exports = deepEqual;
+
+deepEqual({ test: 5 }, { test: 5, extra: null });
+deepEqual({ test: { abc: 5 } }, { test: { def: 5 } });
