5b44742647b5a000eaaf0b124a0973ec4e95efd2
Onisko <Onisko@MacBook.local>
2019-04-08 15:23:29 +0300

    Solution

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -24,23 +24,22 @@ function deepEqual(obj1, obj2) {
     return true;
   }
 
-  if (obj1 === null || typeof (obj1) !== 'object' ||
-      obj2 === null || typeof (obj2) !== 'object') {
+  if (obj1 === null || typeof (obj2) !== 'object') {
     return false;
   }
 
-  var propertiesInObj1 = 0;
-  var propertiesInObj2 = 0;
+  var numPropertiesInObj1 = 0;
+  var numPropertiesInObj2 = 0;
   for (var property in obj1) {
-    propertiesInObj1 += 1;
+    numPropertiesInObj1 += 1;
   }
   for (property in obj2) {
-    propertiesInObj2 += 1;
+    numPropertiesInObj2 += 1;
     if (!(property in obj1) || !deepEqual(obj1[property], obj2[property])) {
       return false;
     }
   }
-  return propertiesInObj1 === propertiesInObj2;
+  return numPropertiesInObj1 === numPropertiesInObj2;
 }
 
 module.exports = deepEqual;
