676e2f87396b510dfeae4b47683f3bbd751834b1
yojikyo <yojikyo@gmail.com>
2019-04-08 18:39:25 +0300

    'solution'

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -21,6 +21,39 @@
  */
 function deepEqual(a, b) {
   // write code here
+  let typeOfA = typeof a;
+  let typeOfB = typeof b;
+
+  if (typeOfA === typeOfB && typeOfA !== null) {
+    if (typeOfA === 'object') {
+      if (!isEqualObjectsByKeys(a, b)) {
+        return false;
+      }
+      if (!isEqualObjectsByKeys(b, a)) {
+        return false;
+      }
+      return true;
+    } else {
+      return a === b;
+    }
+  } else {
+    return a === null && b === null;
+  }
+}
+
+function isEqualObjectsByKeys(a, b) {
+  for (let key in a) {
+    if (typeof a[key] === 'object') { // (*) Доступ к ключю a.key видит undefined... Почему так?
+      if (!deepEqual(a[key], b[key])) {
+        return false;
+      }
+    } else {
+      if (a[key] !== b[key]) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 module.exports = deepEqual;
