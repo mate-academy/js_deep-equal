bc6b10031ecc103c245215452e0d1bef67547c64
VladimirTi <tytmes@mail.ru>
2019-04-08 20:35:12 +0300

    solution

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -16,11 +16,28 @@
  * deepEqual('10', 10) === false
  * deepEqual(0, false) === false
  * deepEqual({test: 5}, {test: 5}) === true
- * deepEqual({test: {abc: 5}}, {test: {abc: 5}}) === true
+ * deepEqual(
+ a = {
+ test: {
+    abc: 5
+    }
+ }, {
+ test: {abc: 5}}) === true
  * deepEqual({test: {abc: 5}}, {test: {abc: 5, def: 4}}) === false
  */
-function deepEqual(a, b) {
-  // write code here
-}
 
+function deepEqual(a, b) {
+  if ((a === null && b === null) || typeof (a) !== 'object' || typeof (b) !== 'object') {
+    return a === b;
+  }
+  if (a !== null && b !== null && Object.keys(a).length === Object.keys(b).length) {
+    for (let key in a) {
+      if (!deepEqual(a[key], b[key])) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+}
 module.exports = deepEqual;
