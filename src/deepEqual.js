20d205a056c7b0369a96af81521690e6f8072320
Olegus <gulldan1996@gmail.com>
2019-04-09 22:44:04 +0300

    change _getKeysLength

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -25,13 +25,13 @@
 
 function deepEqual(a, b) {
   const _isObj = arg => typeof arg === 'object' && arg !== null;
-  const _getKeysLength = arg => Object.keys(arg);
+  const _getKeysLength = arg => Object.keys(arg).length;
 
   if (a === b) {
     return true;
   }
   if (_isObj(a) && _isObj(b)) {
-    if (_getKeysLength(a).length !== _getKeysLength(b).length) {
+    if (_getKeysLength(a) !== _getKeysLength(b)) {
       return false;
     }
     for (let prop in a) {
