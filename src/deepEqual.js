bb0c199c4ecce64ff3589120104a4ba9088bb5f4
NikitaKiev <nikita.pukhlenko@gmail.com>
2019-04-11 10:06:57 +0300

    deep equal

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -20,7 +20,18 @@
  * @return {boolean}
  */
 function deepEqual(a, b) {
-  // write code here
+  if (a === b) return true;
+  else if (typeof a === 'object' && a !== null && typeof b === 'object' && b !== null) {
+    if (Object.keys(a).length !== Object.keys(b).length) return false;
+    else {
+      for (let prop in a) {
+        if (b.hasOwnProperty(prop)) {
+          if (!deepEqual(a[prop], b[prop])) return false;
+        } else return false;
+      }
+      return true;
+    }
+  } else return false;
 }
 
 module.exports = deepEqual;
