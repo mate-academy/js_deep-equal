fcb091195d8c447a58c1a8a2432bd0884fec8d35
VladyslavSak <vladyslav.sak@gmail.com>
2019-08-20 17:09:05 +0300

    Solution

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -20,7 +20,27 @@
  * @return {boolean}
  */
 function deepEqual(a, b) {
-  // write code here
+  if (typeof a !== typeof b) {
+    return false;
+  } else if (typeof a !== 'object') {
+    return a === b;
+  } else if (a === null & b === null) {
+    return true;
+  } else if (a === null || b === null) {
+    return false;
+  }
+
+  if (a instanceof Object && b instanceof Object) {
+    const firstParamKeys = Object.keys(a).sort();
+    const secondParamKeys = Object.keys(b).sort();
+
+    for (const key in firstParamKeys) {
+      if (!deepEqual(a[firstParamKeys[key]], b[secondParamKeys[key]])) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 module.exports = deepEqual;
