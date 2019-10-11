caadd42cbf69e8298ed4fc7dfa6396b4263ff75d
Vitalii Starodub <Vitalii.Starodub@playtech.com>
2019-10-11 17:45:41 +0300

    'solution_vitalii'

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -20,7 +20,44 @@
  * @return {boolean}
  */
 function deepEqual(a, b) {
-  // write code here
+  typesCopmaring(a, b);
+  functionsComparing(a, b);
+  if (a instanceof Object && b instanceof Object) {
+    if (countProps(a) !== countProps(b)) {
+      return false;
+    }
+    for (const key in a) {
+      const r = deepEqual(a[key], b[key]);
+      if (!r) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return a === b;
+  }
+}
+
+function countProps(obj) {
+  let count = 0;
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+function typesCopmaring(a, b) {
+  if (typeof a !== typeof b) {
+    return false;
+  }
+}
+
+function functionsComparing(a, b) {
+  if (typeof (a) === 'function') {
+    return a.toString() === b.toString();
+  }
 }
 
 module.exports = deepEqual;
