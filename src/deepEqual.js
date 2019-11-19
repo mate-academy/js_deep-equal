17e94b78efb5f76972217102f7187cab271ecfe8
bandura <banduraweb@ukr.net>
2019-11-19 10:49:27 +0200

    Solution

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -20,7 +20,21 @@
  * @return {boolean}
  */
 function deepEqual(a, b) {
-  // write code here
+  if ((typeof a !== 'object' || typeof b !== 'object')
+    || (a === null || b === null)) {
+    return a === b;
+  }
+  if (typeof a === 'object' && typeof b === 'object') {
+    if (Object.keys(a).length !== Object.keys(b).length) {
+      return false;
+    }
+    for (const key in a) {
+      if ((!Reflect.has(b, key)) || (!deepEqual(a[key], b[key]))) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 module.exports = deepEqual;
