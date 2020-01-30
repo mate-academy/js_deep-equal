830fd6d1d704500650ddcb6d2a1d77ddf71f1725
Olexander <kravchenko.sania@gmail.com>
2020-01-30 19:10:06 +0200

    attempt1

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -20,7 +20,31 @@
  * @return {boolean}
  */
 function deepEqual(a, b) {
-  // write code here
+  if (a === b) {
+    return true;
+  }
+
+  if (typeof a !== typeof b
+    || !(typeof a === 'object' || typeof b === 'function')
+    || (a === null) !== (b === null)
+  ) {
+    return false;
+  }
+
+  const keysA = Object.keys(a);
+  const keysB = Object.keys(b);
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  for (const key of keysA) {
+    if (!b.hasOwnProperty(key) || !deepEqual(a[key], b[key])) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = deepEqual;
