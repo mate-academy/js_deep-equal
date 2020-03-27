7a38ca505b572b1cd4d02a1ec53f4eed599f06ce
mgrinko <glukym@gmail.com>
2020-03-27 17:53:42 +0200

    Linter was disabled in tests

--- src/deepEqual.test.js
+++ src/deepEqual.test.js
@@ -1,92 +1,99 @@
 'use strict';
 
+/* eslint-disable */
 const deepEqual = require('./deepEqual');
 
 const testObj = { name: 'Misha', order: { price: 20 } };
 const testObjPropertiesInDifferentOrder = {
   order: { price: 20 },
-  name: 'Misha'
+  name: 'Misha',
 };
 const testObjExtraNullProperty = {
   name: 'Misha',
   order: { price: 20 },
-  extraField: null
+  extraField: null,
 };
 const testObjChangedProperty = {
   order: { price: 20 },
-  name: 'Petya'
+  name: 'Petya',
 };
 const testObjChangedPropertyInNestedObj = {
   name: 'Misha',
-  order: { price: 1000 }
+  order: { price: 1000 },
 };
 const testObjExtraNullPropertyInNestedObj = {
   name: 'Misha',
-  order: { price: 20, extraField: null }
+  order: { price: 20, extraField: null },
 };
 
 const deepObject = {
   name: 'Misha',
-  order: { price: 20,
+  order: {
+    price: 20,
     count: 1,
     taxes: {
-      vat: { name: 'vat', amount: { uah: 10, usd: 0.37 } }
+      vat: { name: 'vat', amount: { uah: 10, usd: 0.37 } },
     },
     total: {
       withoutTaxes: { uah: 20, usd: 0.74 },
-      withTaxes: { vat: { uah: 30, usd: 1.11 } }
-    }
+      withTaxes: { vat: { uah: 30, usd: 1.11 } },
+    },
   }
 };
 
 const deepObjectPropsInDifferentOrder = {
   name: 'Misha',
-  order: { count: 1,
+  order: {
+    count: 1,
     price: 20,
     taxes: {
-      vat: { name: 'vat', amount: { uah: 10, usd: 0.37 } }
+      vat: { name: 'vat', amount: { uah: 10, usd: 0.37 } },
     },
     total: {
       withTaxes: { vat: { uah: 30, usd: 1.11 } },
-      withoutTaxes: { usd: 0.74, uah: 20 }
-    }
+      withoutTaxes: { usd: 0.74, uah: 20 },
+    },
   }
 };
 
 const deepObjectExtraNullProperty = {
   name: 'Misha',
-  order: { price: 20,
+  order: {
+    price: 20,
     count: 1,
     taxes: {
-      vat: { name: 'vat', amount: { uah: 10, usd: 0.37 } }
+      vat: { name: 'vat', amount: { uah: 10, usd: 0.37 } },
     },
     total: {
       withoutTaxes: { uah: 20, usd: 0.74 },
-      withTaxes: { vat: { uah: 30, usd: 1.11, eur: null } }
-    }
+      withTaxes: { vat: { uah: 30, usd: 1.11, eur: null } },
+    },
   }
 };
 
 const deepObjectChangedProperty = {
   name: 'Misha',
-  order: { price: 20,
+  order: {
+    price: 20,
     count: 1,
     taxes: {
-      vat: { name: 'vat', amount: { uah: 10, usd: 0.37 } }
+      vat: { name: 'vat', amount: { uah: 10, usd: 0.37 } },
     },
     total: {
       withoutTaxes: { uah: 20, usd: 575 },
-      withTaxes: { vat: { uah: 30, usd: 1.11, eur: null } }
-    }
+      withTaxes: { vat: { uah: 30, usd: 1.11, eur: null } },
+    },
   }
 };
 
 test('5 and 5 should be equal', () => {
-  expect(deepEqual(5, 5)).toBe(true);
+  expect(deepEqual(5, 5))
+    .toBe(true);
 });
 
 test('null and null should be equal', () => {
-  expect(deepEqual(null, null)).toBe(true);
+  expect(deepEqual(null, null))
+    .toBe(true);
 });
 
 test(
@@ -107,20 +114,24 @@ test(
   () => {
     expect(deepEqual(
       { test: testObj },
-      { test: testObjPropertiesInDifferentOrder }))
+      { test: testObjPropertiesInDifferentOrder }
+    ))
       .toBe(true);
   });
 
 test('5 and 6 should NOT be equal', () => {
-  expect(deepEqual(5, 6)).toBe(false);
+  expect(deepEqual(5, 6))
+    .toBe(false);
 });
 
 test('0 and false should NOT be equal', () => {
-  expect(deepEqual(0, false)).toBe(false);
+  expect(deepEqual(0, false))
+    .toBe(false);
 });
 
 test('null and 5 should NOT be equal', () => {
-  expect(deepEqual(null, 5)).toBe(false);
+  expect(deepEqual(null, 5))
+    .toBe(false);
 });
 
 test(
@@ -129,16 +140,18 @@ test(
     - a = ${JSON.stringify(testObj)},
     - b = null`,
   () => {
-    expect(deepEqual(testObj, null)).toBe(false);
+    expect(deepEqual(testObj, null))
+      .toBe(false);
   });
 
 test(
   `Object and its copy with extra null property should not be equal
-  input: 
+  input:
     - a = ${JSON.stringify(testObj)},
     - b = ${JSON.stringify(testObjExtraNullProperty)}`,
   () => {
-    expect(deepEqual(testObj, testObjExtraNullProperty)).toBe(false);
+    expect(deepEqual(testObj, testObjExtraNullProperty))
+      .toBe(false);
   });
 
 test(
@@ -147,7 +160,8 @@ test(
     - a = ${JSON.stringify(testObj)},
     - b = ${JSON.stringify(testObjChangedProperty)}`,
   () => {
-    expect(deepEqual(testObj, testObjChangedProperty)).toBe(false);
+    expect(deepEqual(testObj, testObjChangedProperty))
+      .toBe(false);
   });
 
 test(
@@ -158,7 +172,8 @@ test(
   () => {
     expect(deepEqual(
       testObj,
-      testObjChangedPropertyInNestedObj))
+      testObjChangedPropertyInNestedObj
+    ))
       .toBe(false);
   });
 
@@ -171,7 +186,8 @@ test(
   () => {
     expect(deepEqual(
       testObj,
-      testObjExtraNullPropertyInNestedObj))
+      testObjExtraNullPropertyInNestedObj
+    ))
       .toBe(false);
   });
 
@@ -183,7 +199,8 @@ test(
   () => {
     expect(deepEqual(
       deepObject,
-      deepObjectExtraNullProperty))
+      deepObjectExtraNullProperty
+    ))
       .toBe(false);
   });
 
@@ -195,7 +212,8 @@ test(
   () => {
     expect(deepEqual(
       deepObject,
-      deepObjectPropsInDifferentOrder))
+      deepObjectPropsInDifferentOrder
+    ))
       .toBe(true);
   });
 
@@ -207,6 +225,7 @@ test(
   () => {
     expect(deepEqual(
       deepObject,
-      deepObjectChangedProperty))
+      deepObjectChangedProperty
+    ))
       .toBe(false);
   });
