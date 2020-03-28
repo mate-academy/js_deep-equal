ab514ee6bb5cbc56016745643534cdd0461636ce
Nick <nikolyaoooyartchuk@gmail.com>
2020-03-28 21:39:34 +0200

    solution

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -20,7 +20,68 @@
  * @return {boolean}
  */
 function deepEqual(a, b) {
-  // write code here
+  let x = {};
+  let y = {};
+
+  if (((typeof (a) === 'object')
+    || (typeof (b) === 'object'))
+    && (a !== null && b !== null)) {
+    if (recurs(a, b)) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    if (a === b) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function recurs(a1, b1) {
+    let isLast = true;
+
+    for (const key in a1) {
+      if (b1.hasOwnProperty(key)) {
+        if (a1[key] !== b1[key] && typeof (a1[key]) !== 'object') {
+          return false;
+        }
+
+        if (typeof (a1[key]) === 'object' || typeof (b1[key]) === 'object') {
+          isLast = false;
+          x = a1[key];
+          y = b1[key];
+        }
+      } else {
+        return false;
+      }
+    }
+
+    if (Object.keys(a1).length !== Object.keys(b1).length) {
+      for (const key in b1) {
+        if (a1.hasOwnProperty(key)) {
+          if (b1[key] !== a1[key] && typeof (b1[key]) !== 'object') {
+            return false;
+          }
+
+          if (typeof (a1[key]) === 'object' || typeof (b1[key]) === 'object') {
+            isLast = false;
+            x = a1[key];
+            y = b1[key];
+          }
+        } else {
+          return false;
+        }
+      }
+    }
+
+    if (!isLast) {
+      return recurs(x, y);
+    }
+
+    return true;
+  }
 }
 
 module.exports = deepEqual;
