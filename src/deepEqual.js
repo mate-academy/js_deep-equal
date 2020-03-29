adf6e433b6fb4e3a87e4e9500cfa9b10f7d02007
Rustam Khananov <antmine2017@gmail.com>
2020-03-29 11:08:02 +0300

    solution

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -20,7 +20,24 @@
  * @return {boolean}
  */
 function deepEqual(a, b) {
-  // write code here
+  if (typeof (a) !== 'object' || a === null || b === null) {
+    return a === b;
+  };
+
+  const arrA = Object.getOwnPropertyNames(a);
+  const arrB = Object.getOwnPropertyNames(b);
+
+  if (arrA.sort().join('') !== arrB.sort().join('')) {
+    return false;
+  };
+
+  for (const key in a) {
+    if (!deepEqual(a[key], b[key])) {
+      return false;
+    };
+  };
+
+  return true;
 }
 
 module.exports = deepEqual;
