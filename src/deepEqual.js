2eddca90f4ab837388306b57155b8bd9c8c1aa1a
Andrey Faychak <faychak333@gmail.com>
2020-03-31 17:35:31 +0300

    'solution'

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -21,6 +21,41 @@
  */
 function deepEqual(a, b) {
   // write code here
+  const arr = [];
+
+  if (typeof (a) === 'object'
+    && typeof (b) === 'object'
+    && (a !== null && b !== null)) {
+    if (Object.keys(a).length === Object.keys(b).length) {
+      for (const key in a) {
+        if (b.hasOwnProperty(key)) {
+          if (typeof (a[key]) === 'object'
+            && typeof (b[key]) === 'object'
+            && Object.keys(a[key]).length === Object.keys(b[key]).length) {
+            arr.push(deepEqual(a[key], b[key]));
+          } else {
+            if (a[key] !== b[key]) {
+              return false;
+            }
+          }
+        } else {
+          return false;
+        }
+      }
+
+      for (const el of arr) {
+        if (!el) {
+          return false;
+        }
+      }
+
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return (a === b);
+  }
 }
 
 module.exports = deepEqual;
