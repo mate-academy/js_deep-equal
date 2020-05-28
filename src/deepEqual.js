f94e49d52700d5b78da0cf9f6def4e7f3eb66762
Yurii Holiuk <bionicuss@gmail.com>
2020-05-28 09:55:42 +0300

    Revert "Solution"

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -20,35 +20,7 @@
  * @return {boolean}
  */
 function deepEqual(a, b) {
-  if (typeof a === typeof b && typeof a === 'object') {
-    if (a === null || b === null) {
-      return a === b;
-    } else {
-      return compareObjects(a, b);
-    }
-  } else {
-    return a === b;
-  }
-}
-
-function compareObjects(a, b) {
-  let equal;
-
-  if (Object.keys(a).length !== Object.keys(b).length) {
-    return false;
-  }
-
-  for (let item in a) {
-    if (typeof a[item] === typeof b[item] && typeof a[item] === 'object') {
-      equal = compareObjects(a[item], b[item]);
-    } else {
-      equal = (a[item] === b[item]);
-    }
-    if (!equal) {
-      return equal;
-    }
-  }
-  return equal;
+  // write code here
 }
 
 module.exports = deepEqual;
