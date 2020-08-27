5e8fc973c4c7aa3f1ef09c82d3ba8042d039271d
Elena-brv <leena.brv@gmail.com>
2020-08-27 22:03:42 +0300

    solution

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -20,7 +20,35 @@
  * @return {boolean}
  */
 function deepEqual(a, b) {
-  // write code here
+  if (a === null && b === null) {
+    return true;
+  } else if ((a === null && b !== null) || (a !== null && b === null)) {
+    return false;
+  }
+
+  if (typeof a !== 'object' || typeof b !== 'object') {
+    return a === b;
+  }
+
+  if (Object.keys(a).length !== Object.keys(b).length) {
+    return false;
+  }
+
+  for (const key in a) {
+    if (!b.hasOwnProperty(key)) {
+      return false;
+    } else if (typeof (a[key]) !== 'object' && a[key] !== b[key]) {
+      return false;
+    } else if (typeof (a[key]) === 'object') {
+      const result = deepEqual(a[key], b[key]);
+
+      if (!result) {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
 
 module.exports = deepEqual;
