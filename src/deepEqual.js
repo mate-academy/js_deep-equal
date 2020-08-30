f2332339b4188bfe6abead4147a62db6e4e5a357
nanccyy <lidiapriymak@gmail.com>
2020-08-30 19:52:23 +0300

    done

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -20,7 +20,26 @@
  * @return {boolean}
  */
 function deepEqual(a, b) {
-  // write code here
+  if (Object.is(a, b)) {
+    return true;
+  }
+
+  if (typeof a !== 'object' || typeof b !== 'object') {
+    return false;
+  }
+
+  if (a === null || b === null) {
+    return false;
+  }
+
+  const keysA = Object.keys(a);
+  const keysB = Object.keys(b);
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  return keysA.every(key => deepEqual(a[key], b[key]));
 }
 
 module.exports = deepEqual;
